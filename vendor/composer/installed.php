b704561482d7d43c93ad10d232554d074207bcad
Dhairya Bhatia <dhairyabhatia@Dhairyas-MacBook-Air-3.local>
2023-12-22 03:04:48 -0800

    Composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-username/your-repository',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '356a959f30dc027ea8134d73cf53eb3525a23bdf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-username/your-repository' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '356a959f30dc027ea8134d73cf53eb3525a23bdf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
